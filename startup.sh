f08e063190dc64282c337faecf551c08a088e682
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2022-07-26 00:58:59 +0700

    update heroku yaml file

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
-# npm i -g prisma
-# npx prisma db push
-# npx prisma generate
-# npx prisma migrate dev --name init --preview-feature
-# npx prisma db seed
+npm i -g prisma
+npx prisma db push
+npx prisma generate
+npx prisma migrate dev --name init --preview-feature
+npx prisma db seed
 npm run dev
